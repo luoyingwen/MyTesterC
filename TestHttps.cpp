6f38f526d935fab82d5a20e1b7c677b9fae99c8b
Yingwen <luoyingwen@gmail.com>
2021-05-31 16:38:44 +0800

    query system model and hardware id.

--- TestHttps.cpp
+++ TestHttps.cpp
@@ -5,7 +5,7 @@
 void TestHttpsRequest(const TCHAR* szParam)
 {
 	LogStringMessage(__WFILE__);
-	if (RequestAccessRight("lOfP5emffSO0Wq7Hxd0Kd7XIBLf8wCri", "LMhc4AzhypKofTbfNDXyYLRp0iKKF8gL", "com.lenovotab.camera"))
+	if (RequestAccessRight("lOfP5emffSO0Wq7Hxd0Kd7XIBLf8wCri", "LMhc4AzhypKofTbfNDXyYLRp0iKKF8gL"))
 	{
 		LogStringMessage(L"Request successfully");
 
@@ -29,5 +29,21 @@ void TestHttpsVerify(const TCHAR* szParam)
 	}
 }
 
+void TestHttpsGetSystemModel(const TCHAR* szParam)
+{
+	LogStringMessage(__WFILE__);
+	CString value;
+	if (QuerySystemModel(value))
+	{
+		const TCHAR* szValue = value;
+		LogMessage(L"System model = %s", szValue);
+	}
+	else
+	{
+		LogStringMessage(L"Query System modle failed");
+	}
+}
+
 COMMAND_DEFINE(Test, TestHttpsRequest); 
-COMMAND_DEFINE(Test, TestHttpsVerify);
+COMMAND_DEFINE(Test, TestHttpsVerify); 
+COMMAND_DEFINE(Test, TestHttpsGetSystemModel);
--- TestItmes/httpsHeader/OcrVerify.h
+++ TestItmes/httpsHeader/OcrVerify.h
@@ -27,11 +27,88 @@ bool HasValidAccessRight()
     }
 }
 
-bool RequestAccessRight(const char* ak, const char* sk, const char* packageName)
+void LogErrorMessage(const TCHAR* _Format, ...)
 {
-    if (packageName == nullptr || strlen(packageName) > MAX_PATH)
+    if (_Format == NULL)
+        return;
+
+    va_list l;
+    va_start(l, _Format);
+
+    TCHAR szLine[MAX_PATH + 4];
+    int count = _vsntprintf_s(szLine, MAX_PATH, MAX_PATH - 1, _Format, l);
+    if (count <= 0)
+        _tcscat_s(szLine, MAX_PATH + 4, _T("..."));
+    else
+        szLine[MAX_PATH] = '\0';
+    va_end(l);
+
+    ::OutputDebugStringW(szLine);
+}
+
+bool QueryStringValue(HKEY hKeyParent, LPCTSTR lpszKeyName, LPCTSTR pszValueName, CString& value)
+{
+    CRegKey regKey;
+    LONG lRet = regKey.Open(hKeyParent, lpszKeyName, KEY_READ);
+    if (lRet == ERROR_SUCCESS)
     {
-        ::OutputDebugStringW(L"Invalid package name");
+        TCHAR buffer[MAX_PATH + MAX_PATH];
+        DWORD dwLen = sizeof(buffer) / sizeof(TCHAR);
+        lRet = regKey.QueryStringValue(pszValueName, buffer, &dwLen);
+        if (lRet == ERROR_SUCCESS)
+        {
+            value = buffer;
+        }
+        else
+        {
+            LogErrorMessage(L"regKey.QueryStringValue for %s failed, code = %x.", pszValueName, lRet);
+        }
+    }
+    else
+    {
+        LogErrorMessage(L"Open RegKey : %s failed", lpszKeyName);
+    }
+    return lRet == ERROR_SUCCESS;
+}
+
+bool QuerySystemModel(CString& value)
+{
+    return QueryStringValue(HKEY_LOCAL_MACHINE,
+        L"SYSTEM\\CurrentControlSet\\Control\\SystemInformation",
+        L"SystemProductName",
+        value);
+}
+
+bool QueryHardwareId(CString& value)
+{
+    return QueryStringValue(HKEY_LOCAL_MACHINE,
+        L"SYSTEM\\CurrentControlSet\\Control\\SystemInformation",
+        L"ComputerHardwareId",
+        value);
+}
+
+
+bool RequestAccessRight(const char* ak, const char* sk)
+{
+    CString systemModel;
+    CString hardwareId;
+    if (!QuerySystemModel(systemModel))
+        return false;
+
+    systemModel = L"com.lenovotab.camera"; //TODO:  test only
+    if (!QueryHardwareId(hardwareId))
+        hardwareId = "unknown_hardwareId";
+
+    const wchar_t* szPackageName = systemModel;
+    if (szPackageName == nullptr || wcslen(szPackageName) > MAX_PATH)
+    {
+        ::OutputDebugStringW(L"Invalid system model");
+        return false;
+    }
+
+    if (ak == nullptr || strlen(ak) > MAX_PATH || sk == nullptr || strlen(sk) > MAX_PATH)
+    {
+        ::OutputDebugStringW(L"Invalid ak & sk");
         return false;
     }
 
@@ -48,9 +125,11 @@ bool RequestAccessRight(const char* ak, const char* sk, const char* packageName)
     // Post headers.
     wstring headers;
     headers.append(L"smartcamera-app-id:");
-    headers.append(utf82wstr(packageName));
+    headers.append(szPackageName);
     headers.append(L"\r\n");
-    headers.append(L"smartcamera-device-id:234\r\n");
+    headers.append(L"smartcamera-device-id:");
+    headers.append(hardwareId);
+    headers.append(L"\r\n");
     headers.append(L"Content-Type: application/x-www-form-urlencoded\r\n");
     headers.append(L"Content-Length: %d\r\n");
     wchar_t szHeaders[MAX_PATH * 10] = L"";
